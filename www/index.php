d14be7cb9f25013ac02aa564871d7ea14eb5d321
Amethist <info@amethistdevelopingpeople.nl>
2012-04-15 23:20:30 +0200

    default select first archive

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ require_once(ROOT.'/includes/all.php');
 
 if (isset($_GET['clear_session'])) unset($_SESSION);
 
-$aid = get('aid', '', '?aid='); # Archive ID
+$aid = get('aid', '?aid=1', '?aid='); # Archive ID
 $fid = get('fid', '', '&fid='); # Folder ID
 $pid = get('pid', '', '&pid='); # Path ID
 $vid = get('vid', '', '&vid='); # Version ID
