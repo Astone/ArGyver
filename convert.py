c15c2ac511a24f915dcd6f656057c20be7d66acd
Amethist <info@amethistdevelopingpeople.nl>
2012-04-16 21:07:30 +0200

    new convert.py

--- convert.py
+++ convert.py
@@ -5,7 +5,9 @@ import sqlite3
 
 data      = "src-data    : /home/amethist/data/%(date)s/"
 outlook   = "src-outlook : /home/amethist/outlook/%(date)s/"
-thuis     = "src-thuis   : /home/amethist/backups_oud/thuis/"
+thuis_oud = "src-thuis   : /home/amethist/backups_oud/thuis/"
+thuis     = "src-thuis   : /home/amethist/thuis/"
+website   = "src-website : /var/www/amethist/"
 
 configs = [
 ( 1, '2010-08-01', [data, outlook]),
@@ -17,7 +19,7 @@ configs = [
 ( 7, '2011-02-01', [data]),
 ( 8, '2011-03-01', [data]),
 ( 9, '2011-04-01', [data]),
-(10, '2011-05-01', [data]),
+(10, '2011-05-01', [data, thuis_oud]),
 (11, '2011-06-01', [data]),
 (12, '2011-07-01', [data]),
 (13, '2011-08-01', [data]),
@@ -34,30 +36,35 @@ configs = [
 (24, '2012-04-04', [data]),
 (25, '2012-04-05', [data]),
 (26, '2012-04-06', [data]),
-(27, '2012-04-07', [data]),
-(28, '2012-04-08', [data]),
-(29, '2012-04-09', [data]),
+(27, '2012-04-07', [data, outlook]),
+(28, '2012-04-08', []),
+(29, '2012-04-09', []),
 (30, '2012-04-10', [data, outlook]),
 (31, '2012-04-11', [data, outlook]),
-(32, '2012-04-12', [data, outlook, thuis]),
-(33, '2012-04-13', [data, outlook]),
+(32, '2012-04-12', [data, outlook]),
+(33, '2012-04-13', [outlook]),
 (34, '2012-04-14', [data, outlook]),
-(35, '2012-04-15', [data, outlook])]
+(35, '2012-04-15', [data, outlook, thuis, website])]
 
 fp = file('config/convert.cfg.tpl', 'r')
 base = fp.read()
 fp.close()
 
 for (i, date, sources) in configs:
-    output = base
+    print "\n\n"
+    print date + "  : " + str(datetime.now())
+    output = ''
     for source in sources:
         output += source % {'date': date} + "\n"
+    print output
     fp = file('config/convert.cfg', 'w')
-    fp.write(output)
+    fp.write(base + output)
     fp.close()
-    os.system('./argyver.py -c config/convert.cfg -v 3 -l convert.log -ll 5')
+
+    continue;
+
+    os.system('./argyver.py -c config/convert.cfg -v 3')
     db = sqlite3.connect('/home/amethist/backup_new/.data.sqlite')
-    db.cursor.execute('UPDATE iterations SET time = ? WHERE id = ?', mktime(datetime.strptime(date, '%Y-%m-%d')), i)
+    db.cursor().execute('UPDATE iterations SET start = ? WHERE id = ?', (mktime(datetime.strptime(date, '%Y-%m-%d').timetuple())+27*60*60, i))
     db.commit()
     db.close()
-
