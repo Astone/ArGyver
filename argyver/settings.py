94f7fa06e244cfd4f595f370fe109c0c9d48bb6d
Bram Stoeller (Amethist Backup) <bram@amethist.nl>
2015-01-09 23:58:05 +0100

    Settings

--- argyver/settings.py
+++ argyver/settings.py
@@ -68,9 +68,5 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    AGV_DATA_DIR,
-)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
